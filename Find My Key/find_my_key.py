a4cbd56de2ed3059b170e1734d6ea2dc2a69bf6c
Pattharadanai Sanitjairak <6322771724@g.siit.tu.ac.th>
2024-03-03 23:29:04 +0700

    Create find_my_key.py

---
+++ Find My Key/find_my_key.py
@@ -0,0 +1,94 @@
+from hashlib import sha256 as abcdef
+
+hijklmn = "aekanut"
+
+xqc = "flag{r3v3r23_"
+xqb = "xxxxxxx"
+xxx = "}"
+
+xxxyyyy = {"ayaya":"input"}
+jajaja = print
+hahahaha = ""
+vola = False
+
+def _0x0x00000():
+    global hahahaha
+    hahahaha = eval(xxxyyyy["ayaya"]+"(\""+("What is your key? ")+"\")")
+
+def _0x0x000f():
+    global hahahaha
+    wowza = 0
+    try:
+        for k in xqc:
+            if k != hahahaha[wowza]:
+                jajaja("Key is invalid!")
+                return vola 
+            
+            wowza +=1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[6]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[1]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[3]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[4]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[2]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[0]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+
+        if hahahaha[wowza] != abcdef(hijklmn.encode()).hexdigest()[6]:
+            jajaja("Key is invalid!")
+            return vola
+        
+        else:
+            wowza += 1
+            
+        for k in xxx:
+
+            if k != hahahaha[wowza]:
+                jajaja("Key is invalid!")
+                return vola
+            
+            wowza += 1
+
+        jajaja("Your key is valid!")
+        jajaja("Congaturation! Please submit",hahahaha,"as your solution")
+
+    except IndexError as e:
+        jajaja("Key is invalid!")
+        return vola
+    
+_0x0x00000()
+_0x0x000f()
